c34ba627c43e8347d0f6757f142331356949995a
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-09-01 18:12:30 +0900

    test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725180843',resources: [
+    cacheName: 'chirpy-1725181929',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
