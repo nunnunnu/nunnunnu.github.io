e6eb132ac09f9e6ec9c8530fbbf5f0d4cece05fc
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-09-01 22:34:31 +0900

    gemfile update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725181929',resources: [
+    cacheName: 'chirpy-1725197647',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
