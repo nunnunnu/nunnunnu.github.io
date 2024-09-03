b150e76f62eababa275dc402363dc428e944899d
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-09-03 13:25:20 +0900

    syntax color update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725336957',resources: [
+    cacheName: 'chirpy-1725337494',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
