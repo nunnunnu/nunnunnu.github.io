ce12153d201bcaaef279d4a964c6f70c7952778d
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-09-07 22:34:07 +0900

    video test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725712119',resources: [
+    cacheName: 'chirpy-1725716012',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
