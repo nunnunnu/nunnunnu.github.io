b41dbd66b48cd8af435db889a71a4022fd3d0c8b
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-09-08 21:37:40 +0900

    path delete

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725798467',resources: [
+    cacheName: 'chirpy-1725799011',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
