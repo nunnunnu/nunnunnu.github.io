cd7c8b7d33a4b7273313dc6577418534ed798201
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-09-08 17:15:11 +0900

    spring basic upload

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725781306',resources: [
+    cacheName: 'chirpy-1725783246',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
