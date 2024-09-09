cc127024111ed7b4536561cce73843372d35086c
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-09-09 12:33:24 +0900

    path update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725852252',resources: [
+    cacheName: 'chirpy-1725852768',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
